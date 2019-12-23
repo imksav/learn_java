1621b35aac07529c5021ec8c3d19e8887beabd91
imksav <58361148+imksav@users.noreply.github.com>
2019-12-23 12:44:57 +0545

    Create Assignment1Q2SwitchCase.java
    Short Assignment 1 Q.N.2 Solution

---
+++ Assignment1Q2SwitchCase.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+public class Assignment1Q2SwitchCase {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Enter a character to check either it is 'Vowel or 'Consonant:- ");
+		Scanner sc = new Scanner(System.in);
+		char character = sc.next(). charAt(0);
+		
+		char choice = character;
+		switch(choice) {
+		case 'A':
+		System.out.println("The character is 'Vowel'.");
+		break;
+		case 'a':
+			System.out.println("The character is 'Vowel'.");
+			break;
+			
+		case 'E':
+			System.out.println("The character is 'Vowel'.");
+			break;
+		case 'e':
+			System.out.println("The character is 'Vowel'.");
+			break;
+			
+		case 'I':
+			System.out.println("The character is 'Vowel'.");
+			break;
+		case 'i':
+			System.out.println("The character is 'Vowel'.");
+			break;
+			
+		case 'O':
+			System.out.println("The character is 'Vowel'.");
+			break;
+		case 'o':
+			System.out.println("The character is 'Vowel'.");
+			break;
+			
+		case 'U':
+			System.out.println("The character is 'Vowel'.");
+			break;
+		case 'u':
+			System.out.println("The character is 'Vowel'.");
+			break;
+			
+		default:
+			System.out.println("The character is 'Consonant'.");
+		}
+	}
+}
